ebe86e924b60ede9ca18fd9cd6b3b7e0e1af2301
martinmorondo1 <128125058+martinmorondo1@users.noreply.github.com>
2024-04-26 15:15:05 -0300

    add project

---
+++ JS/main.js
@@ -0,0 +1,15 @@
+function openUp() {
+	// Puts our <section id='cover'> element in a variable
+	const $opencover = document.querySelector('#cover');
+	
+	// Adds a `class` attribute with value `animate` to our cover element
+	$opencover.classList.add('animate');
+	
+	//OUTPUT:
+	// <section id='cover' class='animate'>
+}
+
+function playMusic() {
+    var audio = new Audio('/assets/img/videoplayback (online-audio-converter.com).mp3');
+    audio.play();
+}
